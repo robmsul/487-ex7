7d5c642784e112e54e252edc03f6c20020645b82
Rob Sulaski <robmsul@live.unc.edu>
2018-10-24 09:26:31 -0400

    Wrote code but table content does not appear in the page.

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,49 @@ $(document).ready(function(){
    If the grant has no description, do not include it in the table.
 */
 
-
+  var grantsAwarded; // the data
+  var html = ''; // the html that I will build
+  var url = 'NEH_Grants2010s.xml'; // the XML file
+  var title = ''; // the Project Title: col 1
+  var year = ''; // Year Awarded: col 2
+  var amount = ''; // Original Amount: col 3
+  var description = ''; // Grant Description: col 4
+  
+  $.ajax({
+    type: 'GET',
+    url: url,
+    data: grantsAwarded,
+    dataType: 'xml',
+    async: true,
+    success: function(grantsAwarded){
+      console.log('success');
+      
+      // the table headers
+      html += '<tr>';
+      html += '<th>Project Title</th>';
+      html += '<th>Year Awarded</th>';
+      html += '<th>Original Amount</th>';
+      html += '<th>Project Description</th>';
+      html += '</tr>';
+      
+      // function to parse data
+      $(grantsAwarded).find('Grant').each(function(){
+        title = $(this).find('ProjectTitle');
+        year = $(this).find('YearAwarded');
+        amount = $(this).find('OriginalAmount');
+        description = $(this).find('ToSupport');
+        html += '<tr>';
+        html += '<td>' + title + '</td>';
+        html += '<td>' + year + '</td>';
+        html += '<td>' + amount + '</td>';
+        html += '<td>' + description + '</td>';
+        html += '</tr>';
+        
+      });
+      
+      $('#results').append(html); // tell jQuery to insert the results
+    }
+    
+  });
+  
 });
